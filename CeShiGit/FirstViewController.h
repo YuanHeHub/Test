9c4ae8214c7609302a2cda6c6114864e0cb07b30
YuanHeHub <15237107499@163.com>
2020-04-17 16:30:36 +0800

    100创建了一个文件

---
+++ CeShiGit/FirstViewController.h
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.h
+//  CeShiGit
+//
+//  Created by 元和 on 2020/4/17.
+//  Copyright © 2020年 元和. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirstViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CeShiGit/FirstViewController.m
@@ -0,0 +1,32 @@
+//
+//  FirstViewController.m
+//  CeShiGit
+//
+//  Created by 元和 on 2020/4/17.
+//  Copyright © 2020年 元和. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
