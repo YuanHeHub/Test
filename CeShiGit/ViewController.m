6ae6543dd858ac1f14f9ad00cd65be001ac7f649
YuanHeHub <15237107499@163.com>
2020-04-20 16:38:06 +0800

    计算了股数

--- CeShiGit/ViewController.m
+++ CeShiGit/ViewController.m
@@ -20,6 +20,15 @@
     /**
      第一次提交
      */
+    
+    //股数计算：a=规模金额/现价,a=((int)(a/100))*100
+    //        NSInteger scaleInt = [self.scaleStr integerValue];
+    NSInteger scaleInt = 30 * 10000;
+    CGFloat latestPriceFlo = [@"6.59" floatValue];
+    CGFloat a = scaleInt / latestPriceFlo;
+    NSInteger aa = ((NSInteger)(a / 100)) * 100;
+    NSString *pilesNumberStr = [NSString stringWithFormat:@"%ld",(long)aa];
+    NSLog(@"======%@",pilesNumberStr);
 }
 
 
